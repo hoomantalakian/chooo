622a3abe3742d4c56314ad3d39fd66439a972c68
hoomantalakian <hooman.talakian@gmail.com>
2023-05-29 17:08:05 +0330

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'چو دیزاین',
+  title: 'چــو دیزاین',
   description: 'فروشگاه مبلمان چوبی',
 }
 
