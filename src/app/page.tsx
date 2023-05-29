d92ab04a441244592680be2f4269a6b95837d49f
hoomantalakian <hooman.talakian@gmail.com>
2023-05-29 19:22:31 +0330

    bg darkened

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
 	return (
 		<main>
 			<h1 className="font-extrabold text-6xl text-slate-50">چــو دیزاین</h1>
-      <p className="pt-5 text-slate-50">جادوی هزاره سوم</p>
+      <p className="pt-5 text-slate-50">جادوی هزاره ســوم</p>
 		</main>
 	);
 }
