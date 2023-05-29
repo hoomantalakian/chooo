35b64a5c187e14ebd8bda1baed7090907a2bc0e5
hoomantalakian <hooman.talakian@gmail.com>
2023-05-29 18:43:11 +0330

    dummy homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<main>
 			<h1 className="font-extrabold text-6xl text-slate-50">چــو دیزاین</h1>
+      <p className="pt-5 text-slate-50">جادوی هزاره سوم</p>
 		</main>
 	);
 }
