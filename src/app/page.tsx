c1bb50c08c1a9b97c0c385b367c09310c063d0ef
hoomantalakian <hooman.talakian@gmail.com>
2023-05-29 17:56:11 +0330

    simple hello page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<p>Hello</p>
+			<h1 className="font-extrabold text-6xl text-slate-50">چــو دیزاین</h1>
 		</main>
 	);
 }
