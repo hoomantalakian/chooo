f83fa9f679b79c7d293f60f8566ff30cbb87eec0
hoomantalakian <hooman.talakian@gmail.com>
2023-05-30 00:09:35 +0330

    simple © footer

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+	return (
+		<header>
+			<ul id="menu">
+				<li>خانه</li>
+				<li>محصولات</li>
+				<li>گالری</li>
+				<li>درباره</li>
+			</ul>
+		</header>
+	);
+}
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
+import Header from "../../components/Header";
+
 export default function Home() {
 	return (
-		<div id="BODY" className=" text-slate-50">
-			<header >
-        <ul id="menu">
-          <li>خانه</li>
-          <li>محصولات</li>
-          <li>گالری</li>
-          <li>درباره</li>
-        </ul>
-      </header>
+		<div id="Body" className=" text-gray-50">
+			<Header></Header>
 			<main>
-				<h1 className="font-extrabold text-6xl ">
-					چــو دیزاین
-				</h1>
+				<h1 className="font-extrabold text-6xl ">چــو دیزاین</h1>
 				<p className="pt-5">جادوی هزاره ســوم</p>
 			</main>
+			<footer>
+				<p className="text-center text-gray-400">© Choo Design 2023 | Developerd by codEnd</p>
+			</footer>
 		</div>
 	);
 }
